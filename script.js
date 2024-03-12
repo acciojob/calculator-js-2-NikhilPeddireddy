0b07face3e4f70c4713570e1710c02b102ad4291
NikhilPeddireddy <139708292+NikhilPeddireddy@users.noreply.github.com>
2024-03-12 17:18:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+  document.querySelectorAll('.btn').forEach(item => {
+        item.addEventListener('click', event => {
+            if (item.id === 'C') {
+                document.getElementById('display').value = '';
+            } else if (item.id === 'equal') {
+                try {
+                    document.getElementById('display').value = eval(document.getElementById('display').value);
+                } catch (error) {
+                    document.getElementById('display').value = 'Error';
+                }
+            } else if (item.id === 'back') {
+                let currentValue = document.getElementById('display').value;
+                document.getElementById('display').value = currentValue.slice(0, -1);
+            } else {
+                document.getElementById('display').value += item.id;
+            }
+        })
+    });
